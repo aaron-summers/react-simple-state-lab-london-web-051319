d44f3f6836de75b946bacb276a3a1a2be4410c95
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-07-22 11:24:54 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -17,7 +17,7 @@ export default class Cell extends React.Component {
     render() {
         return (
             <div className="cell" style={{backgroundColor: this.state.color}} onClick={() => {
-                console.log('color switched to #333')
+                // console.log('color switched to #333')
                 this.colorSwitch()
             }}>
 
